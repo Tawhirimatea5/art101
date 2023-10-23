16bd374f1ec9b9fb284eebd4a65b657c2117ab37
Vincent <84070740+Tawhirimatea5@users.noreply.github.com>
2023-10-22 22:45:44 -0700

    Lab 4 Actual Submission

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Vincent Stuhlmuller
+// Date: 10/22/2023
 
 // Constants
 
@@ -19,3 +19,30 @@ function main() {
 
 // let's get this party started
 main();
+//Rock Paper Scissors
+
+//Define the game choices (Rock, Paper, Scissors).
+//Initialize player scores (Player1Score and Player2Score).
+//Create a function to display the game menu with choices.
+//Create a function to get the player's choice (Player1Choice).
+//Randomly generate a choice for Player 2 (Player2Choice).
+//Create a function to determine the winner of the round (DetermineRoundWinner).
+//Update the scores based on the round result (UpdateScores).
+//Display the results of the current round (DisplayRoundResults).
+//Check for the game's end condition (GameOver).
+//Display the final game results.
+//Ask if the players want to play again.
+
+Changing Tire:
+
+1. Gather necessary tools and materials
+2. Park the vehicle in a safe and level location
+3. Turn on hazard lights for safety
+4. Locate the spare tire, jack, lug wrench, and wheel chocks
+5. Use the lug wrench to slightly loosen the lug nuts on the flat tire
+6. Place wheel chocks behind the tires to prevent rolling
+7. Position the jack in a safe and stable location under the vehicle
+8. Carefully raise the vehicle with the jack until the flat tire is off the ground
+9. Completely remove the loosened lug nuts and the flat tire
+10. Install the spare tire onto the wheel hub
+
