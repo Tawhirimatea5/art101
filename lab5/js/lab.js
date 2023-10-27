42ec371794a45a866fe964b7c10ba01c156e2570
Vincent <84070740+Tawhirimatea5@users.noreply.github.com>
2023-10-26 20:10:14 -0700

    Lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -2,20 +2,26 @@
 // Author: Your Name
 // Date:
 
-// Constants
+// Define Variables
+make = "Tesla"
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+model = "Cybertruck"
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
+color = "Space gray"
+
+
+year = 2023;
+
+
+// calculate
+age = 2030 - year;
+
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
