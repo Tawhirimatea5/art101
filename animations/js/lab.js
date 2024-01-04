ac39d6d884d55366309d85ac188401ab06232b37
Vincent <84070740+Tawhirimatea5@users.noreply.github.com>
2024-01-04 00:37:37 -0800

    Portfolio Transition

---
+++ animations/js/lab.js
@@ -0,0 +1,27 @@
+// index.js - purpose and description here
+// Author: Your Name
+// Date:
+
+// Define Variables
+make = "Tesla"
+
+
+model = "Cybertruck"
+
+
+color = "Space gray"
+
+
+year = 2023;
+
+
+// calculate
+age = 2030 - year;
+
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
